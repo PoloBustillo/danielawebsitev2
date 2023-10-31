5503270c687214638fb270bc7f1e808e20f97b40
PoloBustillo <leopoldobeguiluz1@hotmail.com>
2023-10-30 21:52:49 -0600

    metadata

---
+++ danielawebsitev2/src/utils/constants.ts
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+export const metadataPsic: Metadata = {
+  title: "Psicologa Daniela Diaz",
+  description: "Servicios de Terapias psicológicas al mejor precio.",
+  applicationName: "Psicologa Daniela Diaz en Puebla",
+  generator: "Next.js",
+  keywords: "psicologa, Daniela Diaz, blog de cuidado mental, terapias",
+  openGraph: {
+    type: "website",
+    url: "https://psic-danieladiaz.com/",
+    title: "Psicologa Daniela Diaz",
+    description: "Terapias al mejor precio",
+    siteName: "Servicios psicologicos",
+    images: [
+      {
+        url: "/assets/logo/logo500.webp",
+      },
+    ],
+  },
+};
