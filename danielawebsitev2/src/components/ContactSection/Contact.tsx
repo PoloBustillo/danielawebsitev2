f1a1ac7754d3e743a998beeed979c43d27e14530
PoloBustillo <leopoldobeuiluz1@hotmail.com>
2023-11-03 21:20:32 -0600

    populate contact

--- danielawebsitev2/src/components/ContactSection/Contact.tsx
+++ danielawebsitev2/src/components/ContactSection/Contact.tsx
@@ -1,6 +1,10 @@
+import { getWebData } from "@/lib/api";
+import { WebDataType } from "@/lib/types";
 import React from "react";
 
-const Contact = () => {
+const Contact = async () => {
+  const webData = (await getWebData()) as WebDataType;
+  console.log(webData);
   return (
     <section className="py-6 mt-20 dark:bg-gray-800 dark:text-gray-50">
       <div className="grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8 md:grid-cols-2 md:divide-x">
@@ -10,7 +14,7 @@ const Contact = () => {
             Llama o manda un mensaje por alguna de mis redes sociales.
           </p>
           <div className="space-y-4">
-            <p className="flex items-center">
+            <p className="flex items-center flex-row">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
@@ -23,7 +27,16 @@ const Contact = () => {
                   clipRule="evenodd"
                 ></path>
               </svg>
-              <span>Fake address, 9999 City</span>
+              <div>
+                {webData.address.map((address) => {
+                  return (
+                    <div className="capitalize">
+                      <span className="font-bold">{address.type}</span>
+                      {`:   ${address.values}`}
+                    </div>
+                  );
+                })}
+              </div>
             </p>
             <p className="flex items-center">
               <svg
@@ -34,7 +47,7 @@ const Contact = () => {
               >
                 <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
               </svg>
-              <span>123456789</span>
+              <span>{webData.telefono}</span>
             </p>
             <p className="flex items-center">
               <svg
@@ -46,14 +59,15 @@ const Contact = () => {
                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
                 <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
               </svg>
-              <span>contact@business.com</span>
+              <span>{webData.email}</span>
             </p>
           </div>
         </div>
         <div className="flex flex-col py-6 space-y-6 md:py-0 md:px-6">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3772.323223481286!2d-98.24784688509992!3d19.00547368712844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x33f973c342c5addd!2sPsic%C3%B3logo%20Daniela%20Diaz%20Merino%20-%20Puebla%20-%20Terapia!5e0!3m2!1sen!2smx!4v1612124170876!5m2!1sen!2smx"
+            src={webData.googleMapUrl}
             style={{ border: "0", height: "35vh" }}
+            className="ml-10"
             allowFullScreen={false}
             aria-hidden="false"
             tabIndex={0}
