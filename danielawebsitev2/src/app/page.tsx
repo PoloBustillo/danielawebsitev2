ca99307f11eb03ca9d55372f6895102a91c608a8
PoloBustillo <leopoldobeguiluz1@hotmail.com>
2023-11-10 23:57:18 -0600

    add carousel

--- danielawebsitev2/src/app/page.tsx
+++ danielawebsitev2/src/app/page.tsx
@@ -7,6 +7,7 @@ import Cards from "@/components/Cards/Cards";
 import Newsletter from "@/components/Footer/Newsletter/Newsletter";
 import Contact from "@/components/ContactSection/Contact";
 import Faq from "@/components/FAQ/Faq";
+import Carousel from "@/components/Carousel/Carousel";
 
 export const dynamic = "force-dynamic";
 
@@ -24,6 +25,7 @@ const page = async () => {
       <main>
         <Banner lema={lema} frase={frase} />
         <Cards terapias={terapias}></Cards>
+        <Carousel></Carousel>
         <Contact></Contact>
         <Faq></Faq>
       </main>
