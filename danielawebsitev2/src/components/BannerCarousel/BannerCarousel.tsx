3cc9bd9c057d9d064cb045ee8f9e0cc5881289e6
PoloBustillo <39663418+PoloBustillo@users.noreply.github.com>
2023-12-13 22:39:46 -0600

    adding markdown

--- danielawebsitev2/src/components/BannerCarousel/BannerCarousel.tsx
+++ danielawebsitev2/src/components/BannerCarousel/BannerCarousel.tsx
@@ -13,7 +13,9 @@ const BannerCarousel = ({ data }: BannerProps) => {
         <div className="container flex flex-col-reverse mx-auto lg:flex-row">
           <div className="flex flex-col px-6 py-8 space-y-6 rounded-sm sm:p-8 lg:p-12 lg:w-1/2 xl:w-2/5 dark:bg-rose-400 dark:text-gray-900">
             <h1 className="mb-6 text-4xl font-bold">{data.title}</h1>
-            <div className="flex space-x-2 sm:space-x-4"></div>
+            <div className="flex space-x-2 sm:space-x-4">
+              <Markdown rehypePlugins={[rehypeRaw]}>{data.content}</Markdown>
+            </div>
           </div>
           <div className="lg:w-1/2 xl:w-3/5 dark:bg-gray-800">
             <div className="flex items-center justify-center p-4 md:p-8 lg:p-12">
