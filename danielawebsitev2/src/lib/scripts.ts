1da0ea8b12c622488a275ed243d2be1720f85128
PoloBustillo <leopoldobeguiluz1@hotmail.com>
2024-01-29 23:20:34 -0600

    add tab cita

--- danielawebsitev2/src/lib/scripts.ts
+++ danielawebsitev2/src/lib/scripts.ts
@@ -1,11 +1,6 @@
 // scripts.js
 export const SimplybookWidgetScript = `
-  var widget = new SimplybookWidget({
-    "widget_type": "button",
-    "url": "https:\/\/psicdaniela.simplybook.me",
-    "theme": "bookingtroll",
-    // ... other settings
-  });
+var widget = new SimplybookWidget({"widget_type":"button","url":"https:\/\/psicdaniela.simplybook.me","theme":"bookingtroll","theme_settings":{"timeline_hide_unavailable":"0","timeline_show_end_time":"0","timeline_modern_display":"as_slots","sb_base_color":"#0a3d65","display_item_mode":"list","sb_review_image":"6","sb_review_image_preview":"\/uploads\/psicdaniela\/image_files\/preview\/801453c79c2bc1a14c066ad82b046c55.png","dark_font_color":"#262020","light_font_color":"#ecf0f9","btn_color_1":"#26DD9C","sb_company_label_color":"#ffffff","hide_img_mode":"0","sb_busy":"#f57070","sb_available":"#f7faff"},"timeline":"modern","datepicker":"top_calendar","is_rtl":false,"app_config":{"allow_switch_to_ada":0,"predefined":[]},"button_title":"Haz tu cita ","button_background_color":"rgb(214 41 123)","button_text_color":"#f5f5f5","button_position":"right","button_position_offset":"55%"});
 `;
 
 export const MessengerTagScript = `
